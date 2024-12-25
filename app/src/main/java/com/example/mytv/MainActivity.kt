4413d1e14461b27051b77d6f4274f98832d0faa4
bobowg163 <bobowg@163.com>
2024-12-25 22:01:54 +0800

    第一次提交

---
+++ app/src/main/java/com/example/mytv/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.mytv
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.tv.material3.ExperimentalTvMaterial3Api
+import androidx.tv.material3.Surface
+import com.example.mytv.ui.theme.MyTvTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalTvMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyTvTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    shape = RectangleShape
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
